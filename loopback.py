d27a645aa7a528b123b766afc154de5aa3fecf00
DiogoPereiraMatos <pg50340@alunos.uminho.pt>
2023-05-31 07:21:24 -0400

    It works

--- loopback.py
+++ loopback.py
@@ -2,18 +2,25 @@
 from __future__ import print_function, absolute_import, division
 
 import logging
+from logging import info
 import os
-
 from errno import EACCES
 from os.path import realpath
 from threading import Lock
 
+# Fuse:
 from fusepy import FUSE, FuseOSError, Operations, LoggingMixIn
+
+# Telegram:
+import telebot
+from threading import Thread
+import random
+import string
 
 
 """
 Setup:
-pip install fusepy
+pip install fusepy pyTelegramBotAPI
 sudo apt install python3-fusepy
 
 Run with:
@@ -27,6 +34,78 @@ class Loopback(LoggingMixIn, Operations):
 	def __init__(self, root):
 		self.root = realpath(root)
 		self.rwlock = Lock()
+
+		self.cached_chat_id = {}
+
+		bot = telebot.TeleBot("5980867026:AAEx5ukcI67CGPOkD0f-qZK565xrLIhf_2Y")
+		self.bot = bot
+		bot_thread = Thread(target=self._start_bot)
+		bot_thread.start()
+
+		@bot.message_handler(commands=['start'])
+		def welcome_message(message):
+			if message.chat.type != "private":
+				bot.send_message(message.chat.id, "**It's not a good idea to use me on a public chat**")
+				return
+
+			user = message.from_user.first_name
+			chat_id = message.chat.id
+
+			if user in self.cached_chat_id:
+				bot.send_message(chat_id, f"""
+					User already cached:
+						user_id: {user}
+						chat_id: {chat_id}
+					"""
+				)
+				return
+
+			self.cached_chat_id[user] = chat_id
+			info(" + " + str((user, chat_id)))
+
+			bot.send_message(chat_id, f"""
+				I'm a simple bot used to send OTP codes for 2FA. I'm **not interactive**.
+				Available commands:
+					- /start to cache user entry if not already cached
+					- /reset to erase cached user entry
+					- /code to request a code (for testing purposes)
+	
+				Entry added:
+					user: {user}
+					chat_id: {chat_id}
+				"""
+			)
+
+		@bot.message_handler(commands=['reset'])
+		def remove_user(message):
+			user = message.from_user.first_name
+			chat_id = self.cached_chat_id[user]
+
+			self.cached_chat_id.pop(user)
+			info(" - " + str((user, chat_id)))
+
+			bot.send_message(message.chat.id, "Entry removed.")
+
+		def _randomword(length):
+			letters_lc = string.ascii_lowercase
+			letters_uc = string.ascii_uppercase
+			numbers = "0123456789"
+
+			return ''.join(random.choice(letters_lc + letters_uc + numbers) for _ in range(length))
+
+		@bot.message_handler(commands=['code'])
+		def send_code(message):
+			user = message.from_user.first_name
+			if user not in self.cached_chat_id:
+				self.bot.send_message(message.chat.id, "User not in cache. Use /start.")
+				return
+			self._send_code(user, _randomword(10))
+
+	def _start_bot(self):
+		self.bot.infinity_polling()
+
+	def _send_code(self, user, code):
+		self.bot.send_message(self.cached_chat_id[user], code)
 
 	def __call__(self, op, path, *args):
 		return super(Loopback, self).__call__(op, self.root + path, *args)
@@ -113,6 +192,6 @@ if __name__ == '__main__':
 	parser.add_argument('mount')
 	args = parser.parse_args()
 
-	logging.basicConfig(level=logging.DEBUG)
+	logging.basicConfig(level=logging.INFO)
 	fuse = FUSE(
 		Loopback(args.root), args.mount, foreground=True, allow_other=True)
